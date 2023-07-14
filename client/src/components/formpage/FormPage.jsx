a49575205bd9027709115a643de36fd1137a2a6b
SUHAIL PARRY <Suhail.parry@mail.mcgill.ca>
2023-07-14 13:12:13 -0400

    submit button added

--- client/src/components/formpage/FormPage.jsx
+++ client/src/components/formpage/FormPage.jsx
@@ -4,6 +4,9 @@ import Template from "./template/Template";
 import MainFormBody from "./mainformbody/MainFormBody";
 import { Routes, Route } from "react-router-dom";
 import FormBuilder from "./createform/FormBuilder";
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
+
 const FormPage = () => {
   return (
     <>
@@ -27,6 +30,26 @@ const FormPage = () => {
           }
         />
       </Routes>
+
+      <Box sx={{ width: "fit-content", margin: "0 auto" }}>
+        <Button
+          type="submit"
+          sx={{
+            backgroundColor: "#9C27B0",
+            color: "white",
+            fontSize: "20px",
+            padding: "10px 60px",
+            borderRadius: "5px",
+            cursor: "pointer",
+            transition: "opacity 0.36s ease", // Add transition property for opacity
+            "&:hover": {
+              backgroundColor: "#BA68C8",
+            },
+          }}
+        >
+          Submit
+        </Button>
+      </Box>
     </>
   );
 };
